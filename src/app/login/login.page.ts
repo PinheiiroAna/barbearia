12150aca279c3e2d6928e238e5683be356e08f43
Ana Beatriz Gomes <flavia.p_17@hotmail.com>
2024-06-04 19:19:56 -0300

    atualização

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../service/auth.service'
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +8,26 @@ import { AuthService } from '../service/auth.service'
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-  email: string = '';
-  password: string = '';
+  email: string='';
+  password: string='';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  login() {
-    this.authService.login(this.email, this.password);
+  async login() {
+    try {
+      await this.authService.signInWithEmail(this.email, this.password);
+      this.router.navigate(['servico']);
+    } catch (error) {
+      this.router.navigate(['/failure']);
+    }
   }
 
-  googleLogin() {
-    this.authService.googleLogin();
+  async loginWithGoogle() {
+    try {
+      await this.authService.signInWithGoogle();
+      this.router.navigate(['/servico']);
+    } catch (error) {
+      this.router.navigate(['/failure']);
+    }
   }
 }
